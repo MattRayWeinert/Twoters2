c6fea66941445ee204f0f4f642f9b10e94a4ae5f
MattRayWeinert <43066793+MattRayWeinert@users.noreply.github.com>
2020-09-14 10:43:26 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } );
+    
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log("MongoDB database connection established successfully");
+    })
+    
+    const newUserRouter = require('./routes/user');
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(
+    cors({
+        origin: "http://localhost:3000", // <-- location of the react app were connecting to
+        credentials: true,
+        maxAge: 60 * 60 * 3600
+    }));
+    app.use(cookieParser(process.env.SECRET));
+    app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+            // store: new MongoStore({ mongooseConnection: dbConnection })
+    }));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    require("./passport-config.js")(passport);
+
+    // Routes
+    app.use('/user', newUserRouter);
+
+    app.listen(port, () => {
+        console.log(`Server is running on port: ${port}`);
+});
